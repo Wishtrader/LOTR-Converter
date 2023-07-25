d8d66b82c2168fd71a962f49b3d0ee2375bdf9a0
Wishtrader <mr.akomarov@gmail.com>
2023-07-26 01:17:05 +0300

    Initial Commit

---
+++ LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by Andrei Kamarou on 26.07.23.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
